a2cbd1827dd61524d4422ed0ee3ef92da5a61fbf
Springbaron <wengkeatw@gmail.com>
2023-12-11 13:37:31 +0800

    Initial commit

---
+++ src/app/SearchBar.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import styles from "./userTable.module.scss";
+
+export default function SearchBar() {
+  return (
+    <main className={styles.searchBar}>
+      <section className={styles.headerSection}>Search</section>
+      <section className={styles.fieldsSection}>
+        <div className={styles.field}>
+          <div>Name</div>
+          <input className={styles.input} type="text" placeholder="Search by name" />
+        </div>
+        <div className={styles.field}>
+          <div>User Role</div>
+          <input className={styles.input} type="text" placeholder="Please Select" />
+        </div>
+        <div className={styles.field}>
+          <div>User ID</div>
+          <input className={styles.input} type="text" placeholder="Search by user ID" />
+        </div>
+        <div className={styles.field}>
+          <div>Status</div>
+          <input className={styles.input} type="text" placeholder="Search by status" />
+        </div>
+        <div className={styles.field}>
+          <div>Department</div>
+          <input className={styles.input} type="text" placeholder="Search by department/branch" />
+        </div>
+      </section>
+      <section className={styles.actionsSection}>
+        <button className={styles.action + " " + styles.secondaryBtn}>Clear</button>
+        <button className={styles.action + " " + styles.primaryBtn}>Search</button>
+      </section>
+    </main>
+  )
+}
+
---
+++ src/app/UserTable.tsx
@@ -0,0 +1,101 @@
+"use client";
+import styles from "./userTable.module.scss";
+import Image from "next/image";
+import {
+  CAvatar,
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCol,
+  CProgress,
+  CRow,
+  CTable,
+  CTableBody,
+  CTableDataCell,
+  CTableHead,
+  CTableHeaderCell,
+  CTableRow,
+} from "@coreui/react";
+import CIcon from "@coreui/icons-react";
+import {
+  cibCcAmex,
+  cibCcApplePay,
+  cibCcMastercard,
+  cibCcPaypal,
+  cibCcStripe,
+  cibCcVisa,
+  cibGoogle,
+  cibFacebook,
+  cibLinkedin,
+  cifBr,
+  cifEs,
+  cifFr,
+  cifIn,
+  cifPl,
+  cifUs,
+  cibTwitter,
+  cilCloudDownload,
+  cilPeople,
+  cilUser,
+  cilUserFemale,
+} from "@coreui/icons";
+import { useState } from "react";
+
+interface Props {
+  items: {
+    name: string;
+    userId: string;
+    department: string;
+    userRole: string;
+    activity: string;
+    status: string;
+    action: string;
+  }[];
+}
+export default function UserTable(props: Props) {
+  return (
+    <main className={styles.userTable}>
+      <section className={styles.headerSection}>
+        <CellHeader>No</CellHeader>
+        <CellHeader>Name</CellHeader>
+        <CellHeader>User ID</CellHeader>
+        <CellHeader>Department/Branch</CellHeader>
+        <CellHeader>User Role</CellHeader>
+        <CellHeader>Activity</CellHeader>
+        <CellHeader>Status</CellHeader>
+        <CellHeader>Action</CellHeader>
+      </section>
+      <section className={styles.usersSection}>
+        {props.items.map((item, index) => {
+          return (
+            <div
+              key={index}
+              className={
+                styles.row + (index % 2 === 0 ? " " + styles.even : "")
+              }
+            >
+              <CellUser>{index + 1}</CellUser>
+              <CellUser>{item.name}</CellUser>
+              <CellUser>{item.userId}</CellUser>
+              <CellUser>{item.department}</CellUser>
+              <CellUser>{item.userRole}</CellUser>
+              <CellUser>{item.activity}</CellUser>
+              <CellUser>{item.status}</CellUser>
+              <CellUser>{item.action}</CellUser>
+            </div>
+          );
+        })}
+      </section>
+    </main>
+  );
+}
+
+interface CellProps {
+  children: React.ReactNode;
+}
+function CellHeader({ children }: CellProps) {
+  return <div className={styles.cell + " " + styles.headerCell}>{children}</div>;
+}
+function CellUser({ children }: CellProps) {
+  return <div className={styles.cell + " " + styles.userCell}>{children}</div>;
+}
