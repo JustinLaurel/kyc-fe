9ca53587948e2ceafc2e58b6193defa92bb7e943
Chiew Weng Keat <wengkeatw@gmail.com>
2024-03-27 18:08:38 +0800

    Vite image optimizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ViteImageOptimizer()],
   resolve: {
     alias: {
       "@/src": path.resolve(__dirname, "./src"),
